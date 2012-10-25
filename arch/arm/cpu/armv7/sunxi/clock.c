b621a43a63c9a5a4376b47a37ab57b63d25bd6ae
Tom Cubie <tangliang@allwinnertech.com>
2012-10-26 00:56:57 +0200

    ARM: sunxi: Basic Allwinner A10/A13 (sun4i/sun5i) support
    This adds support for the Allwinner A10/A13 SoC's. Additionally
    board support for the dev-boards sun4i/sun5i is added.
    
    NOTES (remove in final version before submission):
    Additionally, I did put some comments for further cleanup into the
    code. Search for "test-only" to find those.
    
    And should Tom's email address be changed? He is not working for Allwinner
    any more. Correct?
    
    Signed-off-by: Tom Cubie <tangliang@allwinnertech.com>
    Signed-off-by: Henrik Nordström <henrik@henriknordstrom.net>
    Signed-off-by: Stefan Roese <sr@denx.de>

---
+++ arch/arm/cpu/armv7/sunxi/clock.c
@@ -0,0 +1,43 @@
+/*
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * Tom Cubie <tangliang@allwinnertech.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+#include <common.h>
+#include <asm/io.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+
+int clock_init(void)
+{
+	struct sunxi_ccm_reg *const ccm =
+		(struct sunxi_ccm_reg *)SUNXI_CCM_BASE;
+
+	/* uart clock source is apb1 */
+	sr32(&ccm->apb1_clk_div_cfg, 24, 2, APB1_CLK_SRC_OSC24M);
+	sr32(&ccm->apb1_clk_div_cfg, 16, 2, APB1_FACTOR_N);
+	sr32(&ccm->apb1_clk_div_cfg, 0, 5, APB1_FACTOR_M);
+
+	/* open the clock for uart */
+	sr32(&ccm->apb1_gate, 16 + CONFIG_CONS_INDEX - 1, 1, CLK_GATE_OPEN);
+
+	return 0;
+}
