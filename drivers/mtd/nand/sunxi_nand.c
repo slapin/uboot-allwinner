34b532f7e87da8b4ba2362c05c6d6f22fb4df068
Henrik Nordstrom <henrik@henriknordstrom.net>
2012-11-18 17:37:27 +0100

    mtd sunxi: Attemt to enable randomization

--- drivers/mtd/nand/sunxi_nand.c
+++ drivers/mtd/nand/sunxi_nand.c
@@ -56,8 +56,14 @@ void debug_writel(const char *name, uint32_t r, void *addr) {
 #define NFC_CE_SEL		(7 << 24)
 
 #define NFC_REG_ECC_CTL 	0x01c03034
-#define NFC_ECC_MODE		(0xf << 12)
+#define NFC_ECC_EN		(1 << 0)
+#define NFC_ECC_PIPELINE	(1 << 3)
+#define NFC_ECC_EXCEPTION	(1 << 4)
+#define NFC_ECC_BLOCK_SIZE	(1 << 5)
 #define NFC_RANDOM_EN		(1 << 9)
+#define NFC_RANDOM_DIRECTION	(1 << 10)
+#define NFC_ECC_MODE		(0xf << 12)
+#define NFC_RANDOM_SEED		(0x7fff << 16)
 
 #define NFC_REG_ST		0x01c03004
 #define NFC_CMD_INT_FLAG	(1 << 1)
@@ -139,9 +145,50 @@ static int sunxi_nand_check_rb(int rb)
 	else
 		return 1;
 }
+static unsigned sunxi_nand_random_seed(int page)
+{
+	static const unsigned short random_seed[128] = {
+    0x2b75, 0x0bd0, 0x5ca3, 0x62d1, 0x1c93, 0x07e9, 0x2162, 0x3a72, 0x0d67, 0x67f9, 
+    0x1be7, 0x077d, 0x032f, 0x0dac, 0x2716, 0x2436, 0x7922, 0x1510, 0x3860, 0x5287, 
+    0x480f, 0x4252, 0x1789, 0x5a2d, 0x2a49, 0x5e10, 0x437f, 0x4b4e, 0x2f45, 0x216e, 
+    0x5cb7, 0x7130, 0x2a3f, 0x60e4, 0x4dc9, 0x0ef0, 0x0f52, 0x1bb9, 0x6211, 0x7a56, 
+    0x226d, 0x4ea7, 0x6f36, 0x3692, 0x38bf, 0x0c62, 0x05eb, 0x4c55, 0x60f4, 0x728c, 
+    0x3b6f, 0x2037, 0x7f69, 0x0936, 0x651a, 0x4ceb, 0x6218, 0x79f3, 0x383f, 0x18d9, 
+    0x4f05, 0x5c82, 0x2912, 0x6f17, 0x6856, 0x5938, 0x1007, 0x61ab, 0x3e7f, 0x57c2, 
+    0x542f, 0x4f62, 0x7454, 0x2eac, 0x7739, 0x42d4, 0x2f90, 0x435a, 0x2e52, 0x2064, 
+    0x637c, 0x66ad, 0x2c90, 0x0bad, 0x759c, 0x0029, 0x0986, 0x7126, 0x1ca7, 0x1605, 
+    0x386a, 0x27f5, 0x1380, 0x6d75, 0x24c3, 0x0f8e, 0x2b7a, 0x1418, 0x1fd1, 0x7dc1, 
+    0x2d8e, 0x43af, 0x2267, 0x7da3, 0x4e3d, 0x1338, 0x50db, 0x454d, 0x764d, 0x40a3, 
+    0x42e6, 0x262b, 0x2d2e, 0x1aea, 0x2e17, 0x173d, 0x3a6e, 0x71bf, 0x25f9, 0x0a5d, 
+    0x7c57, 0x0fbe, 0x46ce, 0x4939, 0x6b17, 0x37bb, 0x3e91, 0x76db 
+	};
+
+	return random_seed[page % 128 ];
+}
+
+static void sunxi_nand_enable_random(int page)
+{
+#if RANDOMIZE_IO_DATA
+	u32 ctl = readl(NFC_REG_ECC_CTL);
+	ctl |= NFC_RANDOM_EN;
+	ctl &= ~NFC_RANDOM_DIRECTION;
+	ctl &= ~NFC_RANDOM_SEED;
+	ctl |= sunxi_nand_random_seed(page) << 16;
+	writel(ctl, NFC_REG_ECC_CTL);
+#endif
+}
+
+static void sunxi_nand_disable_random(void)
+{
+	u32 ctl = readl(NFC_REG_ECC_CTL);
+	ctl &= ~NFC_RANDOM_EN;
+	writel(ctl, NFC_REG_ECC_CTL);
+}
+
 /* Command - command to do
  * column - address within page
  * page_addr = page number (row)*/
+
 static void do_nand_cmd(int command, int column, int page_addr)
 {
 	int addr_cycle, wait_rb_flag, data_fetch_flag, byte_count;
@@ -230,6 +277,7 @@ static void sunxi_nand_command(struct mtd_info *mtd, unsigned command,
 	struct nand_chip *nand = mtd->priv;
 	int bufloc, dlen;
 	debug("nand command = %u, col %d. page_addr %d\n", command, column, page_addr);
+	sunxi_nand_disable_random();
 	switch(command) {
 	case NAND_CMD_RESET:
 	case NAND_CMD_READID:
@@ -249,6 +297,7 @@ static void sunxi_nand_command(struct mtd_info *mtd, unsigned command,
 		bufloc = 0;
 		dlen = mtd->writesize;
 		memset(page_buffer, 0, sizeof(page_buffer));
+		sunxi_nand_enable_random(page_addr);
 		for (i = 0; i < mtd->writesize / 1024 + 1; i++) {
 			do_nand_cmd(NAND_CMD_READ0, column, page_addr);
 			do_nand_cmd(NAND_CMD_RNDOUT, column + bufloc, -1);
